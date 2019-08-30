94c1acf3ec6c097b8f7751112fc9acee23181332
Juakata <ha.uzquiano@hotmail.com>
2019-08-30 13:40:02 -0600

    changing board to get the values

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class Board
+  def initialize
+    @map = {}
+    for i in 1..9
+      map[i] = " "
+    end
+  end
+
   def display_board
-    2.times { print "\n    |    |  \n---------------" }
-    print "\n    |    |  \n"
+    i=1
+    3.times do
+      print "\n #{@map[i]} | #{@map[i+1]} | #{@map[i+2]} \n"
+      print "-----------" unless i<=6
+      i+=3
+    end
   end
 end
