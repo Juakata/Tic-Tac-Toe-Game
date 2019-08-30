ec46e9ea13b175b87f66f27686af34cf33dddcb1
Juakata <ha.uzquiano@hotmail.com>
2019-08-30 17:59:26 -0600

    game's logic fixed

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class Board
-  @wins1 = { 1 => [1, 2, 3], 2 => [1, 5, 9], 3 => [1, 4, 7], 4 => [2, 1, 3], 5 => [2, 5, 8], 6 => [3, 1, 2] }
-  @wins2 = { 7 => [3, 5, 7], 8 => [3, 6, 9], 9 => [4, 1, 7], 10 => [4, 5, 6], 11 => [5, 2, 8], 12 => [5, 4, 6] }
-  @wins3 = { 13 => [5, 3, 7], 14 => [5, 1, 9], 16 => [6, 3, 9], 17 => [6, 5, 4], 18 => [7, 1, 4], 19 => [7, 5, 3] }
-  @wins4 = { 20 => [7, 8, 9], 21 => [8, 2, 5], 22 => [8, 7, 9], 23 => [9, 3, 6], 24 => [9, 1, 5], 25 => [9, 7, 8] }
-
   def initialize
+    @wins1 = { 1 => [1, 2, 3], 2 => [1, 5, 9], 3 => [1, 4, 7], 4 => [2, 1, 3], 5 => [2, 5, 8], 6 => [3, 1, 2] }
+    @wins2 = { 7 => [3, 5, 7], 8 => [3, 6, 9], 9 => [4, 1, 7], 10 => [4, 5, 6], 11 => [5, 2, 8], 12 => [5, 4, 6] }
+    @wins3 = { 13 => [5, 3, 7], 14 => [5, 1, 9], 16 => [6, 3, 9], 17 => [6, 5, 4], 18 => [7, 1, 4], 19 => [7, 5, 3] }
+    @wins4 = { 20 => [7, 8, 9], 21 => [8, 2, 5], 22 => [8, 7, 9], 23 => [9, 3, 6], 24 => [9, 1, 5], 25 => [9, 7, 8] }
     @wins = [@wins1, @wins2, @wins3, @wins4]
     @map = {}
     i = 1
@@ -26,20 +25,21 @@ class Board
   end
 
   def add_map(val, symbol)
+    result = false
     if @map[val] == '.'
       @map[val] = symbol
-      return true
+      result = true
     end
-    false
+    result
   end
 
   def check_map(_val, symbol)
     winner = true
     @wins.each do |element|
-      element.each do |value|
+      element.each do |_key, value|
         winner = true
         value.each do |i|
-          winner = false unless map[i] == symbol
+          winner = false unless @map[i] == symbol
         end
         break if winner
       end
