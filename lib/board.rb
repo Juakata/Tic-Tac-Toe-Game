0656d0648891828485fa768624a344e840a72278
Juakata <ha.uzquiano@hotmail.com>
2019-09-02 15:22:29 -0600

    issues with stickler resolved

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Board
-  attr_accessor :winner
-  @@plays = 0
+  attr_accessor :winner, :plays
+
   def initialize
     @wins1 = { 1 => [1, 2, 3], 2 => [4, 5, 6], 3 => [7, 8, 9], 4 => [1, 4, 7] }
     @wins2 = { 5 => [2, 5, 8], 6 => [3, 6, 9], 7 => [1, 5, 9], 8 => [3, 5, 7] }
     @wins = [@wins1, @wins2]
     @map = {}
+    @plays = 0
     i = 1
     9.times do
       @map[i] = '.'
@@ -18,7 +19,7 @@ class Board
 
   def display_board
     i = 1
-    string = ""
+    string = ''
     3.times do
       string += "\n #{@map[i]} | #{@map[i + 1]} | #{@map[i + 2]} \n"
       string += '-----------' if i <= 6
@@ -31,7 +32,7 @@ class Board
     result = false
     if @map[val] == '.'
       @map[val] = symbol
-      @@plays += 1
+      @plays += 1
       result = true
     end
     result
@@ -50,13 +51,5 @@ class Board
       break if win
     end
     win
-  end
-
-  def self.plays
-    @@plays
-  end
-
-  def self.plays=(value)
-    @@plays = value
   end
 end
