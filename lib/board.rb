a577a0bf8f1b7452de9a6dbedb557ff4404ddcdf
dev1980 <brham1980@gmail.com>
2019-09-03 03:14:06 +0545

    error fixed

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,9 @@
 
 class Board
   attr_accessor :winner
-  @@plays = 0
+  
   def initialize
+    @plays = 0
     @wins1 = { 1 => [1, 2, 3], 2 => [4, 5, 6], 3 => [7, 8, 9], 4 => [1, 4, 7] }
     @wins2 = { 5 => [2, 5, 8], 6 => [3, 6, 9], 7 => [1, 5, 9], 8 => [3, 5, 7] }
     @wins = [@wins1, @wins2]
@@ -18,7 +19,7 @@ class Board
 
   def display_board
     i = 1
-    string = ""
+    string = ''
     3.times do
       string += "\n #{@map[i]} | #{@map[i + 1]} | #{@map[i + 2]} \n"
       string += '-----------' if i <= 6
@@ -31,7 +32,7 @@ class Board
     result = false
     if @map[val] == '.'
       @map[val] = symbol
-      @@plays += 1
+      @plays += 1
       result = true
     end
     result
@@ -57,6 +58,6 @@ class Board
   end
 
   def self.plays=(value)
-    @@plays = value
+    @plays = value
   end
 end
