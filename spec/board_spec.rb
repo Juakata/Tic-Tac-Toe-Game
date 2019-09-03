ce7bf188082ee40eff1e92da49607718577cdc22
dev1980 <brham1980@gmail.com>
2019-09-04 02:02:51 +0545

    stickler error fixed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,29 +5,28 @@
 require './lib/game.rb'
 
 RSpec.describe Board do
-
-  let(:board) { board = Board.new }
+  let(:board) { Board.new }
 
   describe '#add_map' do
     it 'returns true if the value can be added to the board' do
-      expect(board.add_map(1, "X")).to eql(true)
+      expect(board.add_map(1, 'X')).to eql(true)
     end
     it 'returns false if the value cannot be added to the board' do
-      board.add_map(1, "X")
-      expect(board.add_map(1, "X")).to eql(false)
+      board.add_map(1, 'X')
+      expect(board.add_map(1, 'X')).to eql(false)
     end
   end
 
   describe '#check_map' do
     it 'returns true if the board has the same symbol inline' do
-      board.add_map(1, "X")
-      board.add_map(2, "X")
-      board.add_map(3, "X")
-      expect(board.check_map("X")).to eql(true)
+      board.add_map(1, 'X')
+      board.add_map(2, 'X')
+      board.add_map(3, 'X')
+      expect(board.check_map('X')).to eql(true)
     end
     it 'returns false if the board does not have the same symbol inline' do
-      board.add_map(1, "X")
-      expect(board.check_map("X")).to eql(false)
+      board.add_map(1, 'X')
+      expect(board.check_map('X')).to eql(false)
     end
   end
 end
